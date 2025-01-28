28e4b2ba075c899db78b43d0d82668c92e0d0885
curiosity-arch <76086739+curiosity-arch@users.noreply.github.com>
2025-01-28 11:57:38 +0000

    update file actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,25 +1,19 @@
 'use server'
 
-import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation"
 
-export async function createData(formData: FormData) {
-    const username = formData.get('username-register');
-    const password = formData.get('password_register');
-    const birthYear = formData.get('birthYear');
+export async function createData() {
+
 }
 
-export async function updateData(formData: FormData) {
+export async function updateData() {
     
 }
 
-export async function deleteData(formData: FormData) {
+export async function deleteData() {
     
 }
 
-export async function loginUser(formData: FormData) {
-    const username = formData.get('username_login');
-    const password = formData.get('password_login');
-
+export async function loginUser() {
     redirect('/dashboard');
 }
