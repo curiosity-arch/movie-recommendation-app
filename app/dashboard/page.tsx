18bce46ee1b0136e0193800ca6d28d597e855b5d
curiosity-arch <76086739+curiosity-arch@users.noreply.github.com>
2025-03-12 04:53:06 +0700

    Delete app directory

--- app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/public/styles/dashboard-styles.module.css"
-
-export default async function Page() {
-    return (
-        <div className={styles.dashboard}>
-            <h1>Home</h1>
-        </div>
-    )
-}
